6b169c4eee03f1bdc1216c4b435d8338eb3367b9
vitalekp <vitalekpushkarov@gmail.com>
2022-05-16 16:44:07 +0300

    babel todo list

---
+++ lesson3-babel-polyffils/task2/src/scripts/tasksGateway.js
@@ -0,0 +1,35 @@
+const baseUrl = "https://6281ec929fac04c6540b9efb.mockapi.io/api/v1/tasks";
+
+export const getTasksList = () => {
+  return fetch(`${baseUrl}`)
+    .then((response) => response.json())
+    .then((tasks) => tasks);
+};
+
+export const createTask = (taskData) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(taskData),
+  });
+};
+
+export const updateTask = (taskId, updatedTaskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(updatedTaskData),
+  });
+};
+
+export const deleteTask = (taskId) => {
+  console.log(taskId);
+
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: "DELETE",
+  });
+};
