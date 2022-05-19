3f12aea33c32795ff1428c8ef306cc23fabd6473
vitalekp <vitalekpushkarov@gmail.com>
2022-05-19 09:53:28 +0300

    export import

---
+++ lesson4-modules/task1/index.js
@@ -0,0 +1,14 @@
+// file path: index.js
+
+// импортируйте все полифилы из папки polyfills
+// импортируйте ф-ции sum и mult под такими же именами, а так же ф-цию calc под именем calcExpression из файла calculator/index.js
+// импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
+// импортируйте ф-цию printProfile из profile/index.js под таким же именем
+
+const calcResult = calcExpression("1 + 2");
+const sumResult = sum(1, 2);
+const multResult = mult(1, 2);
+const userDataPromise = fetchUser("facebook");
+printProfile({ name: "Tom", from: "The World" });
+
+// убедитесь, скрипт выполнился без ошибок
---
+++ lesson4-modules/task1/indexFirst.js
@@ -0,0 +1,20 @@
+// file path: calculator/index.js
+
+// экспортируйте sum и mult как именной export
+
+const sum = (a, b) => {
+  console.log("implementation for sum");
+  // ... implementation
+};
+
+const mult = (a, b) => {
+  console.log("implementation for mult");
+  // ... implementation
+};
+
+// экспортируйте calc как export по умолчанию
+
+const calc = (expression) => {
+  console.log("implementation for calc");
+  // ... implementation
+};
