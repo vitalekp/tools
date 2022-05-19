5b7bb65d2b38f54d0dd0036fc07122e53c56e890
vitalekp <vitalekpushkarov@gmail.com>
2022-05-19 12:15:23 +0300

    export/import

--- lesson4-modules/task1/index.js
+++ lesson4-modules/task1/index.js
@@ -4,8 +4,9 @@
 // импортируйте ф-ции sum и mult под такими же именами, а так же ф-цию calc под именем calcExpression из файла calculator/index.js
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
-import calcExpression, { sum, mult } from "./calculator";
-import fetchUser from "./gateway";
+
+import calcExpression, { sum, mult } from "./calculator/index.js";
+import fetchUser from "./profile/gateway";
 import { printProfile } from "./profile";
 
 const calcResult = calcExpression("1 + 2");
